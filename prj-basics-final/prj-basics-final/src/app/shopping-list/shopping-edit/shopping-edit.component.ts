15c49392f8d5a072dd7bdfc7dc9cd84b1aff9e7f
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2017-10-11 09:45:24 +0200

    after lecture 205

--- prj-basics-final/prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-basics-final/prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ShoppingListService} from '../shopping-list.service';
 import {NgForm} from '@angular/forms';
 import {Ingredient} from '../../shared/ingredient.model';
@@ -10,9 +10,11 @@ import {Subscription} from 'rxjs/Subscription';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) {
   }
@@ -21,6 +23,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription = this.slService.startedEditing.subscribe((index: number) => {
       this.editMode = true;
       this.editedItemIndex = index;
+      this.editedItem = this.slService.getIngredient(index);
+      this.slForm.setValue({
+        name: this.editedItem.name,
+        amount: this.editedItem.amount
+      });
     });
   }
 
@@ -31,6 +38,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(f: NgForm) {
     const value = f.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
   }
 }

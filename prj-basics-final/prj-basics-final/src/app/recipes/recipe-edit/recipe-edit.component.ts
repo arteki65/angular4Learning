38170ed81258370900c4b72979593425f1645e21
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2017-10-12 13:52:43 +0200

    after lecture 211

--- prj-basics-final/prj-basics-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ prj-basics-final/prj-basics-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {FormControl, FormGroup} from "@angular/forms";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,14 +11,39 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
+      this.initForm();
     });
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let imgUrl = '';
+    let description = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      imgUrl = recipe.imagePath;
+      description = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imgUrl': new FormControl(imgUrl),
+      'description': new FormControl(description)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
 }
